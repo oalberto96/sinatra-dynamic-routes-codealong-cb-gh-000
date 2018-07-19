9cce83176f900ae83a4b5e83e5ce748b06e2646f
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-19 00:19:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     "Goodbye, #{params[:name]}"
   end
 
-  get "/multiply" do 
+  get "/multiply/:num1/:num2" do
     num_1 = params[:num1]
     num_2 = params[:num2]
   end
